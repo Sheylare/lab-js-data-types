e0adfae1102b2c68f49578ea696a13aaf22b21c0
Sheyla Arellano <sheylarellanopsico@gmail.com>
2024-06-19 16:09:58 +0200

    lab terminado

--- index.js
+++ index.js
@@ -8,10 +8,15 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
+const tongueTwister = s1 + " "+ s2 +" "+ s3 + " "+ s4 + " " + s5 + " " + s3 +" "+ s2+ " "+ s1 +" " + s4;
+
+
+
 
 
 // Print out the concatenated string
 
+console.log(tongueTwister);
 
 
 
@@ -20,13 +25,20 @@ const s5 = "and";
 *******************************************/
 const part1 = "java";
 const part2 = "script";
+const part1U = part1[part1.length-1].toUpperCase();
+const part2U = part2[part2.length-1].toUpperCase();
+const palabra1 = part1.slice(0,3) + part1U;
+const palabra2 = part2.slice(0,5) + part2U;
+const result = palabra1 + palabra2;
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
+console.log(part1U);
+console.log(part2U);
 
 
 // Print the cameLtaiL-formatted string
 
-
+console.log(result);
 
 
 /*******************************************
@@ -36,9 +48,13 @@ const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
 
+const  porciento = billTotal * 0.15;
+console.log(porciento);
 
 // Print out the tipAmount
+const tipAmount = billTotal + porciento;
 
+console.log(tipAmount);
 
 
 
@@ -47,11 +63,13 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
+let randomNumber= Math.random()*10;
+let randomNumberR = Math.round(randomNumber);
 
 
 // Print the generated random number
 
-
+console.log(randomNumberR);
 
 /*******************************************
     Iteration 3.1 | Booleans
@@ -61,16 +79,31 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // false
 
-const expression2 = a || b;
+console.log(expression1);
 
-const expression3 = !a && b;
+const expression2 = a || b; // true
 
-const expression4 = !(a && b);
 
-const expression5 = !a || !b;
+console.log(expression2);
 
-const expression6 = !(a || b);
+const expression3 = !a && b; // false
 
-const expression7 = a && a;
+console.log(expression3);
+
+const expression4 = !(a && b); // true
+
+console.log(expression4);
+
+const expression5 = !a || !b; // true
+
+console.log(expression5);
+
+const expression6 = !(a || b); // false
+
+console.log(expression6);
+
+const expression7 = a && a; // true
+
+console.log(expression7);
